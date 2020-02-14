097dfab6742dbbcd935f9d78d335bda40bd2eef5
Pochang Lee <pochanglee2008@gmail.com>
2020-02-14 15:13:34 +0800

    setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='linepay',
+    version='0.1.0',
+    author='Pochang Lee',
+    author_email='stupidgod08@yahoo.com.tw',
+    description='Line pay sdk for python',
+    long_description='Line pay sdk for python ',
+    long_description_content_type='text/markdown',
+    url='https://github.com/pochangl/line-pay',
+    install_requires=[
+        'requests',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+    ],
+)
